d07b9929203dcc7f5dbfa61a9d39292ca898ebba
svenk <svenkata1995@gmail.com>
2023-06-27 17:51:55 +0530

    user script with conf file

---
+++ user.sh
@@ -0,0 +1,9 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
