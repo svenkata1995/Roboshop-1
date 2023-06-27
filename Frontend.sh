b2a9b257629e9f8f864f90fd4c9123fb42bfab96
svenk <svenkata1995@gmail.com>
2023-06-27 16:52:26 +0530

    Frontend script with conf file

---
+++ Frontend.sh
@@ -0,0 +1,15 @@
+echo -e "\e[31m>>> Install nginx <<<\e[0m"
+
+yum install nginx -y
+echo -e "\e[33m>>> Start & Enable Nginx service<<<\e[0m"
+
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[34m>>> extracting the file content <<<\e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[33m<<< Restart Nginx Service >>>\e[0m"
+systemctl restart nginx
