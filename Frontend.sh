040cba21fc214a5d7a9400215fa75672e8f63a3a
svenk <svenkata1995@gmail.com>
2023-06-27 19:18:08 +0530

    variable commands

--- Frontend.sh
+++ Frontend.sh
@@ -1,15 +1,4 @@
 
-yum install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-
-echo -e "\e[34m<<< Restart Nginx Service >>>\e[0m"
-Restart Nginx Service
 date = 29/09/23
 echo today date is ${date}
---
+++ variable .sh
@@ -0,0 +1,6 @@
+date = 27/06/23
+echo today date is ${date}
+date = ${date +%f}
+echo today date is ${date}
+Arth = ((3+5-2*3))
+echo Arth = ${Arth}
