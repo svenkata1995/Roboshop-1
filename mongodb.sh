15d835a4f2795841138bdfd174218722cd253137
svenk <svenkata1995@gmail.com>
2023-06-27 17:29:45 +0530

    Mongodb script with conf file

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,5 @@ yum install mongodb-org -y
 echo -e "\e[33m>>> start and enable MongoDB service >>>\e[0m"
 systemctl enable mongod
 systemctl start mongod
-sed -i -e 's|127.0.0.1|0.0.0.0|'/etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl restart mongod
